7e739c6aecf41c9f13e6e00f6b0e45a7d6383c54
Iovesophy <15680172+Iovesophy@users.noreply.github.com>
2022-04-20 01:59:18 +0900

    FIX: example: format

--- example/binarysearch/main.dart
+++ example/binarysearch/main.dart
@@ -2,15 +2,15 @@ void main() {
   print("binary search start");
   final list = <int>[];
   for (var i = 0; i < 100; i++) {
-      list.add(i);
+    list.add(i);
   }
   print("*** result: ${binarySearch(list, 29)} ***");
 }
 
 confirmation(int mid, int guess, int count) {
-  print("[step ${count}]");
-  print("mid:${mid}");
-  print("guess:${guess}");
+  print("[step $count]");
+  print("mid:$mid");
+  print("guess:$guess");
 }
 
 binarySearch(List l, int item) {
@@ -19,7 +19,7 @@ binarySearch(List l, int item) {
   var high = l.length - 1;
 
   while (low <= high) {
-    var mid = ((low + high) / 2).toInt();
+    var mid = ((low + high) ~/ 2).toInt();
     var guess = l[mid];
     confirmation(mid, guess, count);
 
